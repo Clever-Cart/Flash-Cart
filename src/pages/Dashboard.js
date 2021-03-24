f6d35a98102e208022eba5d13837dc1144bd248f
devsalula <saleh.nazih.dev@gmail.com>
2021-03-24 14:05:35 -0300

    Create Product Card

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -3,8 +3,13 @@ import { connect } from 'react-redux';
 import Button from '@material-ui/core/Button';
 
 import Database from '../services/database';
+import Cache from '../services/cache';
+
+import CardProduct from '../components/card-product';
 
 const Dashboard = ({ history, logout, userId }) => {
+  const [products, setProducts] = React.useState([])
+
   const handleClick = async() => {
     const user = Database.get('Users', userId);
     try {
@@ -16,12 +21,22 @@ const Dashboard = ({ history, logout, userId }) => {
     logout();
   }
 
+  React.useEffect(() => {
+    getProducts();
+  }, [])
+
+  const getProducts = () => {
+    const cartId = Cache.retrieve(Cache.KEYS.CART_ID);
+    Database.getProducts(cartId, setProducts);
+  }
+
   return (
     <div>
       <h1>Dashboard</h1>
       <Button variant='contained' color='primary' onClick={handleClick}>
         Sair
       </Button>
+      <CardProduct />
     </div>
   );
 };
